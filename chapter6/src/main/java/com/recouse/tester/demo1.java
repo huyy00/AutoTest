60ac691ff18340ba4609d23554abb08a6122df14
hyy00 <1625942078@qq.com>
2022-03-10 21:49:54 +0800

    moco配置成功

---
+++ chapter6/src/main/java/com/recouse/tester/demo1.java
@@ -0,0 +1,29 @@
+package com.recouse.tester;
+
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class demo1 {
+
+    @Test
+    public void test1(){
+        Assert.assertEquals(1,1);
+    }
+
+    @Test
+    public void test2(){
+        Assert.assertEquals(1,3);
+    }
+
+    @Test
+    public void test3(){
+        Assert.assertEquals("qqq","qqq");
+    }
+
+    public void log(){
+        Reporter.log("展示日志");
+        throw new RuntimeException("抛出异常");
+    }
+}
+
